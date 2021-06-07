c9363e751a20eca3f52c243cb98d7af85800b129
jhsm croonen <info@jcroonen.nl>
2021-06-07 14:59:32 +0200

    gebruik python uit venv

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-cd /Users/orion/Dev/klanten/wur/flaskProject || exit
+#cd /Users/orion/Dev/klanten/wur/flaskProject || exit
+python=/Users/orion/.virtualenvs/venv/flaskProject/bin/python
 export FLASK_APP=app.py
 export FLASK_ENV=development
 export FLASK_DEBUG=0
-python3 -m flask run
-
+$python -m flask run
