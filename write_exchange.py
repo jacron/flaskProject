89636df3ae591d12bae811c2eebb8115b717a4a4
jhsm croonen <info@jcroonen.nl>
2021-06-08 07:29:33 +0200

    path join

--- write_exchange.py
+++ write_exchange.py
@@ -1,5 +1,8 @@
+import os
+
+
 def write_exchange(content, filename, dir_):
-    fle = dir_ + filename
+    fle = os.path.join(dir_, filename)
     with open(fle, "w+") as fp:
         fp.write(content)
     print('written content to', fle)
